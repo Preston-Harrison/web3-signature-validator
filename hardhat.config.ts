f7d78d86900124360010cfd79d55c842f03d598e
Preston Harrison <preston@prestonharrison.com>
2022-05-22 19:26:20 +1000

    update license

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,11 +12,9 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
+    localhost: {
+      url: "http://localhost:8545",
+    }
   },
 };
 
